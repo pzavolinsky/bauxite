f9765923a5a0071597a3249c106a179704df62ae
Patricio Zavolinsky <pzavolinsky@grupovi-da.com>
2014-01-21 13:12:35 -0300

    Source with timeout.

--- actions/source.rb
+++ actions/source.rb
@@ -5,11 +5,13 @@ class Bauxite::Action
 	#
 	# :category: Action Methods
 	def source(text)
-		actual = @ctx.driver.page_source
-		verbose = @ctx.options[:verbose] ? "\nPage source:\n#{actual}" : ''
-		unless actual =~ _pattern(text)
-			raise Bauxite::Errors::AssertionError, "Assertion failed: page source does not match '#{text}'#{verbose}"
+		@ctx.with_timeout Bauxite::Errors::AssertionError do
+			actual = @ctx.driver.page_source
+			verbose = @ctx.options[:verbose] ? "\nPage source:\n#{actual}" : ''
+			unless actual =~ _pattern(text)
+				raise Bauxite::Errors::AssertionError, "Assertion failed: page source does not match '#{text}'#{verbose}"
+			end
+			true
 		end
-		true
 	end
 end
